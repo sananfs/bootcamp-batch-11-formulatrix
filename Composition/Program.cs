9ae46efa9f5d66a07a0bbb93f5f9e7a072a1a808
sananfs <sananfs@gmail.com>
2024-07-11 09:33:03 +0700

    sarapan day 4

--- Composition/Program.cs
+++ Composition/Program.cs
@@ -9,10 +9,6 @@ class Program
 		Pick pick = new Pick("Dunlop", 25000, "Plastic", 7, "Red");
 
 		Guitar guitar = new Guitar(senar, neck, body);
-		guitar.senar = senar;
-		guitar.neck = neck;
-		guitar.body = body;
-		guitar.pick = pick;
 
 		Console.WriteLine(guitar.body.material);
 		guitar.senar.Bend();
