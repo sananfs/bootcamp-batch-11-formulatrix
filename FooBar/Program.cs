1e47bfd56709bc1ca886ff290086b12714cdf0ec
sananfs <sananfs@gmail.com>
2024-07-15 19:25:46 +0700

    FooBar done

---
+++ FooBar/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main()
+    {
+        for (int n = 0; n <= 15; n++)
+        {
+            string a = "";
+
+            if (n != 0 && n % 3 == 0)
+            {
+                a += "Foo";
+            }
+            if (n != 0 && n % 5 == 0)
+            {
+                a += "Bar";
+            }
+            if (a == "")
+            {
+                a = n.ToString();
+            }
+
+            Console.WriteLine(a);
+        }
+    }
+}
