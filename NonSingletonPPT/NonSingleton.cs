bf8b1f3a92b13043fbe66c16d742b84cf7e7575f
sananfs <sananfs@gmail.com>
2024-08-19 07:49:01 +0700

    Adding before using singleton

---
+++ NonSingletonPPT/NonSingleton.cs
@@ -0,0 +1,15 @@
+namespace NonSingletonPPT;
+
+public class NonSingleton
+{
+    // Constructor public sehingga bisa diinstansiasi dari luar
+    public NonSingleton()
+    {
+    }
+
+    // Method untuk melakukan sesuatu
+    public void DoSomething()
+    {
+        Console.WriteLine("Instance is doing something.");
+    }
+}
---
+++ NonSingletonPPT/Program.cs
@@ -0,0 +1,18 @@
+﻿using NonSingletonPPT;
+
+class Program
+{
+    static void Main()
+    {
+        // Membuat instance baru dari NonSingleton
+        NonSingleton instance = new NonSingleton();
+        instance.DoSomething();
+        
+        // Membuat instance baru dari NonSingleton lagi (ini adalah instance yang berbeda)
+        NonSingleton anotherInstance = new NonSingleton();
+        anotherInstance.DoSomething();
+        
+        // Memeriksa apakah kedua instance adalah objek yang sama
+        Console.WriteLine(ReferenceEquals(instance, anotherInstance));
+    }
+}
